4862b7bfc30b63363ede50507557405e2ca383f2
Zemke <flzemke@gmail.com>
2019-05-20 20:45:05 +0200

    Init

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs = require('fs');
+
+module.exports = function (port, pathToMocks) {
+    http.createServer((req, res) => {
+        req.on('error', err => console.error(err.stack));
+
+        const file = `${pathToMocks}/${req.url.substr(1).replace(/\//g, '_')}.json`;
+        if (req.url === '/favicon.ico') {
+            res.statusCode = 204;
+            res.end();
+        } else if (!fs.existsSync(file)) {
+            console.warn(`${file} does not exist.`);
+            res.statusCode = 404;
+            res.end();
+        } else {
+            console.log(`Serving ${file}`);
+            res.setHeader('Content-Type', 'application/json');
+            fs.createReadStream(file).pipe(res);
+        }
+    }).listen(port);
+}
